29f6083e3d9314570c8139584208b9f2e47cc394
gary909 <garywhite61@hotmail.com>
2021-11-25 18:49:02 +0100

    button code now working

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-let fruits = ['Apple', 'Banana', 'Peach', 'Kiwi', 'Lemon']
+let background = ['#55efc4', '#81ecec', '#74b9ff', '#a29bfe', '#dfe6e9'];
 
+//Random number generator
 function getRandomInt(max) {
     var randNum = Math.floor(Math.random() * max);
-    return fruits[randNum];
+    document.body.style.backgroundColor = background[randNum];
 }
 
-// function myFunction(){
-//     alert("hey you clicked me!");
-// }
+// When the button is clicked we do this;
+function myFunction(){
+    // alert("hey you clicked me!");
+    document.body.style.backgroundColor = getRandomInt(5);
+}
 
-console.log(getRandomInt(5));
+document.getElementById("clickMe").addEventListener("click", myFunction);
+
+// console.log(getRandomInt(5));
