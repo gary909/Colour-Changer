7b5c36c63865b058de6bbac4de3cf6b6547d3816
gary909 <garywhite61@hotmail.com>
2021-11-25 20:51:53 +0100

    'styling'

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let background = ['#55efc4', '#81ecec', '#74b9ff', '#a29bfe', '#dfe6e9'];
+// Array of different colours
+let background = ['#55efc4', '#81ecec', '#74b9ff', '#a29bfe', '#dfe6e9', '#00b894', '#00cec9', '#0984e3', '#6c5ce7', '#b2bec3'];
 
 //Random number generator
 function getRandomInt(max) {
@@ -9,7 +10,7 @@ function getRandomInt(max) {
 // When the button is clicked we do this;
 function myFunction(){
     // alert("hey you clicked me!");
-    document.body.style.backgroundColor = getRandomInt(5);
+    document.body.style.backgroundColor = getRandomInt(9);
 }
 
 document.getElementById("clickMe").addEventListener("click", myFunction);
